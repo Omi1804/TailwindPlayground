aa35fc71561aafc84c8bf31869f737e50d2fc081
Om Nigam <om2021064@akgec.ac.in>
2023-12-31 10:10:10 +0530

    adding display

---
+++ src/pages/elements/boxdecoration.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Boxdecoration = () => {
+  return (
+    <div className="m-10">
+      <span className=" box-decoration-slice hover:box-decoration-clone bg-gradient-to-r from-indigo-400 to-pink-400 text-white p-2 text-lg">
+        Hello <br /> world
+      </span>
+    </div>
+  );
+};
+
+export default Boxdecoration;
