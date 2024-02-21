f6bf2a1d4ad0377fd4b4fa1f78e1560188687680
Om Nigam <om2021064@akgec.ac.in>
2024-02-21 10:10:10 +0530

    custom css

---
+++ src/pages/custom.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//Here i have used btn-primary which we have specified in global css
+const Custom = () => {
+  return (
+    <div className=" text-[2rem] text-[blue] border-[5px] border-[blue]">
+      <button className="btn-primary">Click me</button>
+    </div>
+  );
+};
+
+export default Custom;
