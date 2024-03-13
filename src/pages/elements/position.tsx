9ea776cc3082f3ffb1a472857939a5dbf9ed3818
Om Nigam <om2021064@akgec.ac.in>
2024-03-14 01:42:29 +0530

    adding positions

---
+++ src/pages/elements/position.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+
+const position = () => {
+  return (
+    <div className="w-[25rem] h-[20rem] relative overflow-auto border-[2px] border-blue-600 mx-auto my-10">
+      <div>
+        <div className=" text-white py-1 sticky top-0 text-center bg-[#000000cb]">
+          A
+        </div>
+        <div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+        </div>
+        <div className=" text-white py-1 sticky top-0 text-center bg-[#000000cb]">
+          B
+        </div>
+        <div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+        </div>
+        <div className=" text-white py-1 sticky top-0 text-center bg-[#000000cb]">
+          C
+        </div>
+        <div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+          <div className="flex border-b-2 border-b-white items-center p-3 bg-[#1E293B] gap-5 text-white">
+            <img
+              src="/img1.jpg"
+              className="w-[3rem] h-[3rem] rounded-full"
+              alt=""
+            />
+            <strong>Om Nigam</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default position;
